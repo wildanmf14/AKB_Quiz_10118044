e458da3bc898f8b52aa7158804be84b41f58a0c2
wildhevire <wildhevire404@gmail.com>
2021-05-29 12:59:58 +0700

    Finished

---
+++ app/src/main/java/com/wildhevire/quiz10118044/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wildhevire.quiz10118044;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//
+//        Tanggal Pengerjaan : 29/5/2021
+//        Nim : 10118044
+//        Nama : Wildann Muhammad Fikri
+//
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
